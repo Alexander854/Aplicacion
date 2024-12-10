3633d8805c2fe2de2172b04c605bb3ce63f07c43
Alexander854 <alexjoramirez2006@gmail.com>
2024-12-10 18:15:38 -0300

    v 0.3.1(nada pero anda)

--- my-app/src/screens/login.js
+++ my-app/src/screens/login.js
@@ -98,7 +98,7 @@ export default function Login({ navigation }) {
                 </TouchableOpacity>
             </View>
 
-            {loading && <ActivityIndicator size="large" color="#0000ff" />}
+        
         </View>
     );
 }
